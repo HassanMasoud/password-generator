09ed9026229949ef8d28c1b2cec53fbbf760b02f
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-25 21:33:15 +0300

    Set up form event listeners

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const characterAmountRange = document.getElementById("characterAmountRange");
 const characterAmountNumber = document.getElementById("characterAmountNumber");
+const includeUppercaseElement = document.getElementById("includeUppercase");
+const includeNumbersElement = document.getElementById("includeNumbers");
+const includeSymbolsElement = document.getElementById("includeSymbols");
+
+const form = document.getElementById("passwordGeneratorForm");
 
 characterAmountRange.addEventListener("input", syncCharacterAmount);
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercaseElement.checked;
+  const includeNumbers = includeNumbersElement.checked;
+  const includeSymbols = includeSymbolsElement.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+});
 
 function syncCharacterAmount(e) {
   const value = e.target.value;
